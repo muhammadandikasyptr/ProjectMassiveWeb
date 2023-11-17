87a9f6363fe6d320b80a0d5980eca3e8bb363a74
muhammadandikasyptr <2110631250013@student.unsika.ac.id>
2023-11-17 22:08:10 +0700

    mencoba membuat navbar

---
+++ src/pages/navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li><a href="#">home</a></li>        
+        <li><a href="#">Pesan</a></li>        
+        <li><a href="#">Tentang Kami</a></li>  
+        <li><a href="#">kontak</a></li>  
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar;
